c9999ce5f5d51214da0944a2df7b57bde2fb2891
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-20 13:31:54 +0900

    test: 메소드 테스트

--- baseball/src/main.java
+++ baseball/src/main.java
@@ -6,7 +6,8 @@ import java.util.Set;
 public class main {
     public static void main(String[] args) {
 
-
+        String str = "123";
+        str.chars().forEach(i-> System.out.println(i));
     }
     public static void checkDuplicatedNumber (String userInput) {
         Set<String> userInputList = new HashSet<>(List.of(userInput.split("")));
@@ -16,17 +17,4 @@ public class main {
         }
     }
 }
-
-    public static boolean reGameMessage() {
-        System.out.println(REGAME_SUGGESTION_MSG);
-        String reGameInput = Console.readLine();
-        if (reGameInput.equals("1")) {
-            return true;
-        }
-        if (reGameInput.equals("2")) {
-            return false;
-        }
-        throw new IllegalArgumentException(ERR_MSG);
-    }
-
 
